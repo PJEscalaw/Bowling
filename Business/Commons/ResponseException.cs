e19a49d07fff5cdf9da9f4282f110547ac68f64f
Paul Joshua Escalaw <pjescalaw@sprout.ph>
2022-06-29 09:41:31 +0800

    API - Added pipeline behaviors.

---
+++ Business/Commons/ResponseException.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+namespace Business.Commons
+{
+    public class ResponseException : Exception
+    {
+        public int StatusCode { get; set; }
+        public bool Succeeded { get; set; } = false;
+        public new string Message { get; set; }
+        public object Errors { get; }
+        public new object? Data { get; set; }
+        public ResponseException(int statusCode, string message, object errors)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Errors = errors;
+        }
+
+        public ResponseException(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public ResponseException(string message)
+        {
+            StatusCode = (int)HttpStatusCode.BadRequest;
+            Message = message;
+        }
+
+       
+    }
+}
